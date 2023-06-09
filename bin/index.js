27f479d806b84524bf34fabf70be8f94cfc790fb
Alekzandriia <alekzoreillyz@me.com>
2023-06-08 21:49:00 -0400

    Add personal info

--- bin/index.js
+++ bin/index.js
@@ -4,27 +4,55 @@ import chalk from 'chalk'; // pretty colours
 import chalkAnimation from 'chalk-animation'; // animations
 import terminalLink from 'terminal-link'; // clickable links
 
-// use backticks to facilitate line breaks (template literals)
-const link = terminalLink('My Website', 'https://alekzandriia.com');
-// const sleep = (ms=2000) => { new Promise((r) => setTimeout(r,ms))}
-async function welcome() {
-  chalkAnimation.rainbow("Alexandria O'Reilly");
-setTimeout(() => {
-    // Stop the animation, then write on a new line.
-    console.log("aka alekzandriia");
-}, 3000);
+// TIP: use backticks to facilitate line breaks (template literals)
+
+// LINKS
+const username = 'alekzandriia'
+const links = {
+  website: `https://www.${username}.com`,
+  twitter: `https://www.twitter.com/${username}`,
+  github: `https://www.github.com/${username}`,
+  codepen: `https://www.codepen.com/${username}`,
+  dribbble: `https://www.dribbble.com/${username}`
 }
 
-async function message() {
-  console.log(chalk.blue(`
-Hey, How's it going?
-This is a test!
-Text.... text....        <3
-
-${chalk.red("I'm RED")}
-        ${link}
-`))
+// INFO
+const info = {
+  name: "Alex O'Reilly",
+  location: "Canada",
+  humanLang: ["English (native)", "Spanish (In progress)"],
+  compLang: ["HTML", "CSS", "JavaScript", "SQL", "Bash"],
+  tech: ["React", "Tailwind", "Bootstrap", "Express", "MongoDB", "Wordpress", "Figma"],
 }
-await message()
-await welcome()
-await message()
+
+// Message
+const message = "Oh hey there!"
+console.log(message)
+
+// async function message() {
+//   console.log(chalk.blue(`
+// Hey, How's it going?
+// This is a test!
+// Text.... text....        <3
+
+// ${chalk.red("I'm RED")}
+//         ${website}
+// `))
+// }
+
+
+
+const titleAnimation = (title, name) => {
+  const rainbow = chalkAnimation.rainbow(title)
+  setTimeout(()=> {
+    const glitch = chalkAnimation.glitch(name)
+    setTimeout(()=> {
+      glitch.stop()
+    },3000)
+  },3000)
+}
+
+
+//titleAnimation('Business Card', 'alekzandriia')
+
+
